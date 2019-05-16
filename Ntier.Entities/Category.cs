bc2a3e7e18d1921f511775642afae5757f6bb5b0
kaan akdağ <akdag_kaan@hotmail.com>
2019-05-16 13:35:34 +0300

    pushladım yine

---
+++ Ntier.Entities/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ntier.Entities
+{
+    using Core4;
+    using Ntier.Core4.Entities;
+
+    public class Category :CoreEntity
+    {
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+    }
+}
